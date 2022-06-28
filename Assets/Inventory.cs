6e7213d10ac1508118aa91116ebba51c6f7c0447
Fabio-Klinge <93822163+Fabio-Klinge@users.noreply.github.com>
2022-06-28 11:34:24 +0200

    started inventory

---
+++ Assets/Inventory.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+    var items = new List<item>() {
+                new item(){ Id = 1, Name="screw"},
+
+            };
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
